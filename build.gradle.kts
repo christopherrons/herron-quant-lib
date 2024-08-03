7615da2038c45ea0d6c742ca87d32bdfd296a5a2
christopherrons <christopherheron09@gmail.com>
2024-08-03 19:33:49 +0200

    Fix imports

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,81 +1,81 @@
 plugins {
-	java
-	id("maven-publish")
-	id("io.spring.dependency-management") version "1.1.0"
+    java
+    id("maven-publish")
+    id("io.spring.dependency-management") version "1.1.0"
 }
 
 // Project Configs
 allprojects {
-	repositories {
-		mavenCentral()
-		mavenLocal()
-	}
+    repositories {
+        mavenCentral()
+        mavenLocal()
+    }
 
-	apply(plugin = "maven-publish")
-	apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+    apply(plugin = "java-library")
 
-	group = "com.herron.exchange"
-	version = "1.0.0-SNAPSHOT"
-	if (project.hasProperty("releaseVersion")) {
-		val releaseVersion: String by project
-		version = releaseVersion
-	}
+    group = "com.herron.exchange"
+    version = "1.0.0-SNAPSHOT"
+    if (project.hasProperty("releaseVersion")) {
+        val releaseVersion: String by project
+        version = releaseVersion
+    }
 
-	java {
-		sourceCompatibility = JavaVersion.VERSION_21
-		sourceCompatibility = JavaVersion.VERSION_21
-	}
+    java {
+        sourceCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_21
+    }
 
-	publishing {
-		publications {
-			create<MavenPublication>("herron.exchange") {
-				artifactId = project.name
-				// artifact("build/libs/${artifactId}-${version}.jar")
-				from(components["java"])
-			}
-			repositories {
-				mavenLocal()
-			}
-		}
-	}
+    publishing {
+        publications {
+            create<MavenPublication>("herron.exchange") {
+                artifactId = project.name
+                // artifact("build/libs/${artifactId}-${version}.jar")
+                from(components["java"])
+            }
+            repositories {
+                mavenLocal()
+            }
+        }
+    }
 }
 
 
 dependencies {
-	//Project Modules
-	implementation(project(":quantlib"))
+    //Project Modules
+    implementation(project(":quantlib"))
 
-	// Internal Libs
-	implementation(libs.common.api)
-	implementation(libs.common)
+    // Internal Libs
+    implementation(libs.common.api)
+    implementation(libs.common)
 
-	// External Libs
-	implementation(libs.spring.boot.starter.web)
-	implementation(libs.spring.boot.starter.parent)
+    // External Libs
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.parent)
 
-	// External Test Libs
-	testImplementation(testlibs.junit.jupiter.api)
-	testImplementation(testlibs.junit.jupiter.engine)
+    // External Test Libs
+    testImplementation(testlibs.junit.jupiter.api)
+    testImplementation(testlibs.junit.jupiter.engine)
 }
 
 // Tasks
 val releaseDirName = "releases"
 tasks.register<Tar>("buildAndPackage") {
-	dependsOn("clean")
-	dependsOn("build")
-	tasks.findByName("build")?.mustRunAfter("clean")
-	compression = Compression.GZIP
-	archiveExtension.set("tar.gz")
-	destinationDirectory.set(layout.buildDirectory.dir(releaseDirName))
-	from(layout.buildDirectory.file("libs/${rootProject.name}-${version}.jar"))
+    dependsOn("clean")
+    dependsOn("build")
+    tasks.findByName("build")?.mustRunAfter("clean")
+    compression = Compression.GZIP
+    archiveExtension.set("tar.gz")
+    destinationDirectory.set(layout.buildDirectory.dir(releaseDirName))
+    from(layout.buildDirectory.file("libs/${rootProject.name}-${version}.jar"))
 }
 
 tasks {
-	named("build") {
-		dependsOn(named("publishToMavenLocal"))
-	}
+    named("build") {
+        dependsOn(named("publishToMavenLocal"))
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
